a8b97a45c5d6f6d7930f3cfb5b1899388c07d1f0
Aniket Kumar <56200361+30Aniket@users.noreply.github.com>
2021-08-17 19:51:58 +0530

    Update

--- src/dialogue_system/dialogue_manager/dialogue_manager_hrl.py
+++ src/dialogue_system/dialogue_manager/dialogue_manager_hrl.py
@@ -566,7 +566,7 @@ class DialogueManager_HRL(object):
         # self.model = dl_classifier(input_size=len(self.slot_set), hidden_size=256,
         #                            output_size=len(self.disease_symptom),
         #                            parameter=self.parameter)
-        self.model = dl_classifier(input_size=10, hidden_size=256,
+        self.model = dl_classifier(input_size=7, hidden_size=256,
                                    output_size=len(self.disease_symptom),
                                    parameter=self.parameter)
         if self.parameter.get("train_mode") == False:
